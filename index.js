8a9f124c5d6b373313e1654f8fc7ce8c05c2ef7d
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-05 13:30:33 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const friend = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(friend, event) {
+    let messages = [];
+    for (let i = 0; i < friend.length; i++) {
+        messages.push(`Thank you, ${friend[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+writeCards(friend);
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
